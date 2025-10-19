dbf01807f8157299e71667a56c49ba120117a14d
FrancescQuintoAnguera <francescquinto@iesmontsia.org>
2025-10-19 18:47:59 +0200

    Restructure the common modules

--- src/index.php
+++ src/index.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eazy Ride</title>
     <link rel="stylesheet" href="/common/style/common.css">
-    <link rel="stylesheet" href="/common/navBar/styles/navbar.css">
+    <link rel="stylesheet" href="/common/modules/navBar/styles/navbar.css">
     
     <!-- Cargar el Web Component ANTES de usarlo -->
-    <script src="/common/sidebar/scripts/sidebar.js"></script>
+    <script src="/common/modules/sidebar/scripts/sidebar.js"></script>
 </head>
 <body>
-    <?php include_once __DIR__ . '/common/navBar/template/navbar.php'; ?>
-    <?php include_once __DIR__ . '/common/sidebar/templates/sidebar.php'; ?>
+    <?php include_once __DIR__ . '/common/modules/navBar/template/navbar.php'; ?>
+    <?php include_once __DIR__ . '/common/modules/sidebar/templates/sidebar.php'; ?>
     
     <main id="app">Cargando...</main>
     
